c2e5577186f07c2a97219583082c1bfa649f94a2
Josh-Manley <joshmanley45@gmail.com>
2024-07-17 19:00:12 -0600

    first init

---
+++ src/test/java/com/technews/tech_news_java_api/TechNewsJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technews.tech_news_java_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechNewsJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
